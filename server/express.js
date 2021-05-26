f584f119c9ab68fa759151abc907534830ff6e48
neetsunsun19 <neetsunsun19@gmail.com>
2021-05-26 23:08:05 +0800

    The baseline for Express Server setup

--- server/express.js
+++ server/express.js
@@ -1,24 +1,18 @@
 import express from 'express'
 //import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
-import { compress } from "compression";
-import {  cors } from "cors";
-import {  helmet } from "helmet";
-import {  config } from "../config/config";
+import compress from "compression";
+import cors from "cors";
+import helmet from "helmet";
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(compress())
+// secure apps by setting various HTTP headers
 app.use(helmet())
+// enable CORS - Cross Origin Resource Sharing
 app.use(cors())
-
-app.listen(config.port, (err) => {
-    if(err){
-        console.log(err)
-    }
-    console.info('Server started on port %s.', config.port)
-})
 
 export default app
